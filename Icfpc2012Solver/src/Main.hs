643b8c9abba681fc9bf9d86a3192b66a77b2bd3f
Alexander Granin <graninas@gmail.com>
2015-01-24 15:16:18 +0600

    - Emulator is started. 'Undefined' functions added. - 'Main' program is a dummy for further work.

--- Icfpc2012Solver/src/Main.hs
+++ Icfpc2012Solver/src/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
 import Common
+import LabyrinthParser.Parser
+import Runtime.Emulator
+
+import Control.Monad (liftM)
 
 import qualified Testing.ParserTest as T1 
 
@@ -10,8 +14,16 @@ main = do
 
     T1.test
 
-    m001 <- loadMazeFile "001.txt"
+    m001 <- liftM parse (loadMazeFile "001.txt")
     let solution = "DDDLLLLLLURRRRRRRRRRRRDDDDDDDLLLLLLLLLLLDDDRRRRRRRRRRRD"
     
+    --TODO: initial structure
+    case m001 of
+        Left msg -> putStrLn msg
+        Right maze001 -> do
+            let m001_1 = runEmulation maze001 'L'
+            
+            -- TODO
+            putStrLn "Emulation done."
     
     putStrLn "Done."
--- Icfpc2012Solver/src/Runtime/Emulator.hs
+++ Icfpc2012Solver/src/Runtime/Emulator.hs
@@ -1,10 +1,40 @@
 module Runtime.Emulator where
 
 import Runtime.Types
+import qualified Data.Vector as V
 
-data EmulationResult = Success
-                     | Fail
-  deriving (Show, Read, Eq)
+setRobot = undefined
+emptyCell = undefined
+openLift = undefined
+setStone = undefined
 
-runEmulation :: Maze -> String -> EmulationResult
-runEmulation m sol = undefined
+isValidMove = undefined
+
+
+
+moveRobot current@(x, y) 'L' m | isValidMove (x - 1, y) = setRobot (x - 1, y) (emptyCell current m)
+moveRobot _ _ _ = error "Not implemented."
+
+rowHasRobot = undefined
+
+findRobot m = let
+    rowWithRobotIdx = V.findIndex rowHasRobot m
+    
+    in undefined
+
+
+runEmulation :: Maze -> Char -> Maze
+runEmulation m 'L' = moveRobot (findRobot m) 'L' m
+runEmulation m 'R' = moveRobot (findRobot m) 'R' m
+runEmulation m 'U' = moveRobot (findRobot m) 'U' m
+runEmulation m 'D' = moveRobot (findRobot m) 'D' m
+
+{-
+Move left, L, moving the Robot from (x, y) to (x − 1, y).
+• Move right, R, moving the Robot from (x, y) to (x + 1, y).
+• Move up, U, moving the Robot from (x, y) to (x, y + 1).
+• Move down, D, moving the Robot from (x, y) to (x, y − 1).
+• Wait, W, which does nothing.
+• Abort, A, which abandons mine exploration.
+
+-}
