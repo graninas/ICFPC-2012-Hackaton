02182428c88b6027550060102d5ccd4c97e961f5
Alexander Granin <graninas@gmail.com>
2015-01-24 16:45:14 +0600

    - Validation of one lift added to Parser.

--- Icfpc2012Solver/src/LabyrinthParser/Validator.hs
+++ Icfpc2012Solver/src/LabyrinthParser/Validator.hs
@@ -3,15 +3,22 @@ module LabyrinthParser.Validator where
 import Runtime.Types
 import qualified Data.Vector as V
 
-validateOneRobot :: Maze -> Either String Maze
-validateOneRobot m = case V.foldl' robotsCount 0 m of
-    0 -> Left "No robots."
+validateOneObject :: Char -> String -> Maze -> Either String Maze
+validateOneObject obj objname m = case V.foldl' robotsCount 0 m of
+    0 -> Left $ "No " ++ objname ++ "."
     1 -> Right m
-    n -> Left $ "Too many robots: " ++ show n
+    n -> Left $ "Too many " ++ objname ++ ":" ++ show n
   where
     robotsCount n row = V.foldl' checkRobot n row
-    checkRobot n 'R' = n + 1
+    checkRobot n obj' | obj == obj' = n + 1
     checkRobot n _   = n
 
+
+validateOneRobot :: Maze -> Either String Maze
+validateOneRobot = validateOneObject 'R' "robots"
+
+validateOneClosedLift :: Maze -> Either String Maze
+validateOneClosedLift = validateOneObject 'L' "lifts"
+
 validate maze = validateOneRobot maze
-            -- >>= validateOneClosedLift
+            >>= validateOneClosedLift
