40c51467371614d28103a5d184fa2c89e5a7bd54
Alexander Granin <graninas@gmail.com>
2015-01-24 16:57:44 +0600

    Implemented new functionality in Emulator.

--- Icfpc2012Solver/src/Runtime/Emulator.hs
+++ Icfpc2012Solver/src/Runtime/Emulator.hs
@@ -26,7 +26,10 @@ isValidMove pos m = case getCell pos m of
 
 
 moveRobot current@(x, y) 'L' m | isValidMove (x - 1, y) m = setRobot (x - 1, y) (emptyCell current m)
-moveRobot _ _ _ = error "moveRobot not implemented."
+moveRobot current@(x, y) 'R' m | isValidMove (x + 1, y) m = setRobot (x + 1, y) (emptyCell current m)
+moveRobot current@(x, y) 'U' m | isValidMove (x, y - 1) m = setRobot (x, y - 1) (emptyCell current m)
+moveRobot current@(x, y) 'D' m | isValidMove (x, y + 1) m = setRobot (x, y + 1) (emptyCell current m)
+moveRobot _ move _ = error $ "Unsupported move: " ++ [move]
 
 rowHasRobot row = 'R' `V.elem` row
 
@@ -44,13 +47,6 @@ runEmulation m 'L' = moveRobot (findRobot m) 'L' m
 runEmulation m 'R' = moveRobot (findRobot m) 'R' m
 runEmulation m 'U' = moveRobot (findRobot m) 'U' m
 runEmulation m 'D' = moveRobot (findRobot m) 'D' m
+runEmulation m 'W' = m
+runEmulation m 'A' = error "Abort is undefined."
 
-{-
-Move left, L, moving the Robot from (x, y) to (x − 1, y).
-• Move right, R, moving the Robot from (x, y) to (x + 1, y).
-• Move up, U, moving the Robot from (x, y) to (x, y + 1).
-• Move down, D, moving the Robot from (x, y) to (x, y − 1).
-• Wait, W, which does nothing.
-• Abort, A, which abandons mine exploration.
-
--}
